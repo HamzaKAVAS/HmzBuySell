a311973c422b9c3d7943dc4b857441fa11c23627
HamzaKAVAS <hmzkavas@gmail.com>
2025-04-11 18:38:44 +0300

    110425

--- src/test/java/stepDefinitions/US_018_and_US_035_Steps.java
+++ src/test/java/stepDefinitions/US_018_and_US_035_Steps.java
@@ -4,6 +4,7 @@ import config.ConfigReader;
 import io.cucumber.java.en.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import pages.LoginPages;
@@ -67,7 +68,7 @@ public class US_018_and_US_035_Steps {
     public void verifyThatPurchaseHistoryIsVisibleAndActiveInTheLeftSidebar() {
 
         JSUtilities.scrollToElement(driver, userDashboardPage.purchaseHistorySidebarLink);
-        userDashboardPage.purchaseHistorySidebarLink.isDisplayed();
+        Assertions.assertTrue(userDashboardPage.purchaseHistorySidebarLink.isDisplayed());
         ReusableMethods.wait(2);
     }
 
